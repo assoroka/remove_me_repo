f69d030142144118751fc86f7ac99488b6af3aff
assoroka <a.s.soroka@student.itc.khai.edu>
2026-03-23 11:19:54 +0200

    Commit changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,12 @@
 package org.example;
 
-public class Main {
-    
-        IO.println(String.format("Hello and welcome!"));
+import static java.lang.System.out;
 
+public class Main {
+    public static void main(String[] args) {
+        out.println("Програма запущена!");   
         for (int i = 1; i <= 5; i++) {
-            IO.println("i = " + i);
+            out.println("Крок циклу: " + i);
         }
     }
 }
